ebf06640007f76de53f2ffec3167f5994da29d2e
brunofow <bruocamposfow@gmail.com>
2019-06-17 02:39:56 -0300

    Aula 04*

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,58 @@
-var frase = $(".frase").text();
-var numeroDePalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-
-tamanhoFrase.text(numeroDePalavras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
 
-campo.on("input", function() {
-    var conteudo = campo.val();
-    var qtdPalavras = conteudo.split(/\S+/).length -1;
-    $("#contador-palavras").text(qtdPalavras);
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
-    
+$(function() {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function(){
+function atualizaTamanhoFrase () {
+    var frase = $(".frase").text();
+    var numeroDePalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numeroDePalavras);
+}
+
+function inicializaContadores () {
+    campo.on("input", function() {
+        var conteudo = campo.val();
+        var qtdPalavras = conteudo.split(/\S+/).length -1;
+        $("#contador-palavras").text(qtdPalavras);
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+        
+    });    
+}
+
+function inicializaCronometro () {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function(){
     var cronometroID = setInterval(function(){
         tempoRestante--;
         $("#tempo-digitacao").text(tempoRestante);
+        $("#botao-reiniciar").attr("disabled", true);
         if (tempoRestante == 0){
             campo.attr("disabled", true);
             clearInterval(cronometroID);
+            $("#botao-reiniciar").removeAttr("disabled");
         }
 
     }, 1000);
-});
+});    
+}
+
+$("#botao-reiniciar").click(reiniciaJogo);
+
+function reiniciaJogo () {
+    campo.removeAttr("disabled");
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+
+}
+
+
